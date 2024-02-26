613c578e81d01d04dde9ed7e49085a8524907767
Anders Bjerner <abjerner@skybrud.dk>
2024-02-26 12:17:16 +0100

    Yes please!

--- debug.bat
+++ debug.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet build src/Limbo.Umbraco.TwentyThree --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=c:/nuget
+dotnet build src/Limbo.Umbraco.TwentyThree --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=c:\nuget\Umbraco10
